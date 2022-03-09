c38a6464a3782f83b1277dfbf34a16c0cbf0cb65
KimSeungDeok <tmdejr1117@gmail.com>
2022-03-09 16:59:58 +0900

    파이썬 알고리즘 학습

---
+++ BOG/python/2588.py
@@ -0,0 +1,14 @@
+A = int(input())
+B = int(input())
+
+b = []
+for i in str(B):
+    b.append(i)
+# https://go-hard.tistory.com/96
+
+# print(int(b[0]))
+
+print(A * int(b[2]))
+print(A * int(b[1]))
+print(A * int(b[0]))
+print(A * B)
