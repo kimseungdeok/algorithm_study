3fa2d7d3f3a6ee80858724d2da3a2e139279383b
KimSeungDeok <tmdejr1117@gmail.com>
2022-03-10 18:44:53 +0900

    코드 수정 전 커밋

--- BOG/python/2525.py
+++ BOG/python/2525.py
@@ -5,7 +5,7 @@ td = (m+ct)//60 # time division의 약자로 편하게 계산하기위해 따로
 
 if m + ct > 60:
     if t+td > 23:
-
+        print(t+td-24, -((60*td)-(m+ct)))
     elif t+td <= 23:
         print(t+td, (60*td)-(m+ct)) # 몫만 구하기 위해서 // 연산자 사용
 elif m + ct <60:
