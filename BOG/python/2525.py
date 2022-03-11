de58f6cf06bfce292db394a5e6cf0b8405fe0d1d
KimSeungDeok <tmdejr1117@gmail.com>
2022-03-11 18:17:54 +0900

    최종 수정

--- BOG/python/2525.py
+++ BOG/python/2525.py
@@ -1,12 +1,15 @@
-t, m = map(int, input().split(" "))
-ct = int(input()) # cook time의 약자
-td = (m+ct)//60 # time division의 약자로 편하게 계산하기위해 따로 변수를 만듦
+t, m = map(int, input().split(' '))
+ct = int(input())
+ts = m + ct
+td = ts//60
 
-
-if m + ct > 60:
-    if t+td > 23:
-        print(t+td-24, -((60*td)-(m+ct)))
-    elif t+td <= 23:
-        print(t+td, (60*td)-(m+ct)) # 몫만 구하기 위해서 // 연산자 사용
-elif m + ct <60:
-    print(t, m+ct)
+if t + td <=23:
+    if td>=1:
+        print(t+td, ts-(td*60))
+    elif td<1:
+        print(t, m+ct)
+elif t + td > 23:
+    if td>=1:
+        print(t+td-24, ts-(td*60))
+    elif td<1:
+        print(t, m+ct)
