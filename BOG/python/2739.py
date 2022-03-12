bb51c2adccc59c8776370a71b2631d123ab8df6b
KimSeungDeok <tmdejr1117@gmail.com>
2022-03-12 16:01:06 +0900

    구구단 for문 안 사용하고 풀기

---
+++ BOG/python/2739.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+print(str(n),"*","1","=",n*1)
+print(str(n),"*","2","=",n*2)
+print(str(n),"*","3","=",n*3)
+print(str(n),"*","4","=",n*4)
+print(str(n),"*","5","=",n*5)
+print(str(n),"*","6","=",n*6)
+print(str(n),"*","7","=",n*7)
+print(str(n),"*","8","=",n*8)
+print(str(n),"*","9","=",n*9)
+
