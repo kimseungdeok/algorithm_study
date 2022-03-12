a9ee34e528aaefedba65c2068647403f0cd76924
KimSeungDeok <tmdejr1117@gmail.com>
2022-03-12 17:05:19 +0900

    구구단 for문 사용

--- BOG/python/2739.py
+++ BOG/python/2739.py
@@ -1,12 +1,6 @@
-n = int(input())
+input_num = int(input())
+# num_list = [1,2,3,4,5,6,7,8,9]
 
-print(str(n),"*","1","=",n*1)
-print(str(n),"*","2","=",n*2)
-print(str(n),"*","3","=",n*3)
-print(str(n),"*","4","=",n*4)
-print(str(n),"*","5","=",n*5)
-print(str(n),"*","6","=",n*6)
-print(str(n),"*","7","=",n*7)
-print(str(n),"*","8","=",n*8)
-print(str(n),"*","9","=",n*9)
-
+for i in range(1,10):
+    print(input_num,"*",i,"=",input_num*i)
+    # print(i)
