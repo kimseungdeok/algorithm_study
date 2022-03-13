acce5ab4462a3f1f9c3dac6f68dc2264480d6dcd
KimSeungDeok <tmdejr1117@gmail.com>
2022-03-13 22:53:09 +0900

    10950 A+B-3

--- BOG/python/10950.py
+++ BOG/python/10950.py
@@ -1,12 +1,20 @@
+# count = int(input())
+
+ 
+# for i in range(count):
+#     n1, n2 = map(int, input().split(" "))
+#     n1_arr = []
+#     n1_arr.append(n1)
+#     n2_arr = []
+#     n2_arr.append(n2)
+
+# for i in range(count):
+#     print(n1_arr[i]+n2_arr[i])
+
 count = int(input())
 
  
-for i in range(0,count):
+for i in range(count):
     n1, n2 = map(int, input().split(" "))
-    n1_arr = []
-    n1_arr.append(n1)
-    n2_arr = []
-    n2_arr.append(n2)
+    print(n1+n2)
 
-for i in range(0,count):
-    print(n1_arr[i-1]+n2_arr[i-1])
