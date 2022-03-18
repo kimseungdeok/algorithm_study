61493e95accdabda7f33690c90cecff51252acec
KimSeungDeok <tmdejr1117@gmail.com>
2022-03-18 22:48:07 +0900

    11022 A + B -8

---
+++ BOG/python/11022.py
@@ -0,0 +1,7 @@
+import sys
+
+count = int(sys.stdin.readline())
+
+for i in range(count):
+    A, B = map(int, sys.stdin.readline().split(" "))
+    print('Case #',i+1,':',' ', A, " ", "+", " ", B," ","="," ", A+B, " ", sep='')
