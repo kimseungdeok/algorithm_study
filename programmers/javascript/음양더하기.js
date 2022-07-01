2c130571ae4eb660ddf90c35ee3efe7c42d9cff3
KimSeungDeok <tmdejr1117@gmail.com>
2022-07-01 11:30:00 +0900

    finish : 프로그래머스 음양더하기 문제

---
+++ programmers/javascript/음양더하기.js
@@ -0,0 +1,17 @@
+function solution(absolutes, signs) {
+    var answer = 0;
+    let realNum = [];
+    // let sum = 0;
+    for(let i = 0, signsLen = signs.length; i < signsLen; i++){
+        if(signs[i]===false){
+            realNum.push(absolutes[i] * -1);
+        }
+        else {
+            realNum.push(absolutes[i]);
+        }
+        console.log(realNum);
+        answer += realNum[i];
+    }
+    
+    return answer;
+}
