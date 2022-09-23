bb3d024f4ee0a0d310cee8d595cc007a5c4619c8
kimseungdeok <tmdejr1117@gmail.com>
2022-09-23 15:10:18 +0900

    resolved : 10950 풀이 완료

--- BOG/python/10950.py
+++ BOG/python/10950.py
@@ -1,20 +1,10 @@
-# count = int(input())
+import sys
 
- 
-# for i in range(count):
-#     n1, n2 = map(int, input().split(" "))
-#     n1_arr = []
-#     n1_arr.append(n1)
-#     n2_arr = []
-#     n2_arr.append(n2)
+sys.stdin = open('example.txt', 'r')
 
-# for i in range(count):
-#     print(n1_arr[i]+n2_arr[i])
+input = sys.stdin.read().splitlines()
+num = int(input[0])
 
-count = int(input())
 
- 
-for i in range(count):
-    n1, n2 = map(int, input().split(" "))
-    print(n1+n2)
-
+for i in range(1,num+1):
+    print(int(input[i][0])+ int(input[i][2]))
