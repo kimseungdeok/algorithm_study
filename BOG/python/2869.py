c40f3eb4d1818132e93b0d59098e904a2abbae24
kimseungdeok <tmdejr1117@gmail.com>
2022-09-24 16:53:48 +0900

    resolved : 2869 다른 사람 풀이 참조하여 풀이 완료

--- BOG/python/2869.py
+++ BOG/python/2869.py
@@ -2,31 +2,11 @@ import sys
 
 sys.stdin = open('example.txt', 'r')
 
-input = list(map(int,sys.stdin.readline().split()))
+speed, sleep, target = map(int,sys.stdin.readline().split())
 
-speed= input[0] # A
-sleep = input[1] # B
-target = input[2] # V
+day = (target - sleep) / (speed - sleep)
 
-# location = 0
-up = 0
-down = 0
-day = 0
-
-
-while True:
-    day += 1
-    up = speed * day
-    if up - down >= target:
-        break
-    down = sleep * day
-
-
-    
-
-# while location <= target:
-#     location += speed
-#     location -= sleep
-#     day += 1
-
-print(day)
+if day == int(day):
+    print(int(day))
+else:
+    print(int(day)+1)
