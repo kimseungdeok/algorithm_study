fb4ddf69606a6a49c6dfc95f85f11d04ffd9b387
kimseungdeok <tmdejr1117@gmail.com>
2022-09-26 02:17:48 +0900

    resolved : 풀이 수정

--- BOG/python/2628.py
+++ BOG/python/2628.py
@@ -9,40 +9,25 @@ sys.stdin = open('example.txt', 'r')
 # 1 4
 # 0 2
 
-input = sys.stdin.read().split('\n')
+col, row = map(int, input().split())
+rows = [0, row] # 행
+cols = [0, col] # 열
 
-# print(input)
-
-rectangle = input[0].strip().split()
-
-width = int(rectangle[0]) # 가로
-height = int(rectangle[1]) # 세로
-
-# print(width, height) # 10 8
-
-num = int(input[1])
-# print(num) # 3
-
-del input[0:2]
-# print(input) # ['0 3', '1 4', '0 2']
-
-widthLengths = [0, width]
-heightLengths = [0, height]
-
-for cut in input:
-    cutDirection, cutLength = map(int, cut.strip().split())
-    if cutDirection == 0:
-        heightLengths.append(cutLength)
+for _ in range(int(input())):
+    now, num = map(int, input().split())
+    if now:
+        cols.append(num)
     else:
-        widthLengths.append(cutLength)
+        rows.append(num)
 
-heightLengths.sort()
-widthLengths.sort()
+rows.sort()
+cols.sort()
+# print(rows, cols)
 answer = 0
 
-for i in range(len(heightLengths)-1):
-    for j in range(len(widthLengths)-1):
-        answer = max(answer, (heightLengths[i + 1] - heightLengths[i]) * (widthLengths[j + 1] - widthLengths[j]))
+for i in range(len(rows) - 1):
+    for j in range(len(cols) - 1):
+        answer = max(answer, (cols[j + 1] - cols[j]) * (rows[i + 1] - rows[i]))
 
 print(answer)
-        
+
