3b2a06f5ce71d6ba3adb8f80ecb26ecde3a50f11
kimseungdeok <tmdejr1117@gmail.com>
2022-09-26 02:00:40 +0900

    resolved : 다른 풀이 참조하여 2628 풀이 완료

--- BOG/python/2628.py
+++ BOG/python/2628.py
@@ -11,9 +11,9 @@ sys.stdin = open('example.txt', 'r')
 
 input = sys.stdin.read().split('\n')
 
-print(input)
+# print(input)
 
-rectangle = input[0].split()
+rectangle = input[0].strip().split()
 
 width = int(rectangle[0]) # 가로
 height = int(rectangle[1]) # 세로
@@ -26,13 +26,23 @@ num = int(input[1])
 del input[0:2]
 # print(input) # ['0 3', '1 4', '0 2']
 
-cutWidthLengths = []
-cutHeightLengths = []
+widthLengths = [0, width]
+heightLengths = [0, height]
 
 for cut in input:
-    cutDirection, cutLength = map(int, cut.split())
-    print(cutDirection, cutLength)
+    cutDirection, cutLength = map(int, cut.strip().split())
     if cutDirection == 0:
-        cutHeightLengths.append
+        heightLengths.append(cutLength)
+    else:
+        widthLengths.append(cutLength)
+
+heightLengths.sort()
+widthLengths.sort()
+answer = 0
+
+for i in range(len(heightLengths)-1):
+    for j in range(len(widthLengths)-1):
+        answer = max(answer, (heightLengths[i + 1] - heightLengths[i]) * (widthLengths[j + 1] - widthLengths[j]))
+
+print(answer)
         
-    
