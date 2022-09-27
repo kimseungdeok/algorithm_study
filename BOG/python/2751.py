e8a07a31b48ebe31017079f3f07a1a2eaa50b0f9
kimseungdeok <tmdejr1117@gmail.com>
2022-09-27 17:31:58 +0900

    resolved : merge sort 적용하여 정렬 완료

--- BOG/python/2751.py
+++ BOG/python/2751.py
@@ -2,24 +2,49 @@ import sys
 
 sys.stdin = open('example.txt', 'r')
 
-input = list(map(int,sys.stdin.read().strip().split()))
+input = sys.stdin.readline
 
-testCase = input[0]
+n = int(input())
+li = []
 
-input.pop(0)
+for i in range(n):
+    li.append(int(input()))
 
-nums = input
+# 배열을 받아서 절반을 나누어서 왼쪽과 오른쪽에 할당
+# 왼쪽과 오른쪽 모두 다시 sort를 해줌
+# 인자 : 배열
+# 최종적으로 왼쪽과 오른쪽을 합침
+def sort(arr):
+    if len(arr)<2:
+        return arr
+    mid=len(arr)//2
+    left=sort(arr[:mid])
+    right=sort(arr[mid:])
 
-def bubble_sort(arr):
-    n = len(arr)
+    return merge(left, right)
 
-    for i in range(n):
-        
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
 
-bubble_sort(nums)
+# 왼쪽과 오른쪽 배열을 합쳐주는 함수
+def merge(left, right):
+    new_list=[]
+    i=0
+    j=0
 
-for num in nums:
-    print(num)
+    while (i<len(left)) & (j<len(right)):
+        if left[i]>right[j]:
+            new_list.append(right[j])
+            j += 1
+        else:
+            new_list.append(left[i])
+            i += 1
+    while (j<len(right)):
+        new_list.append(right[j])
+        j += 1
+    while (i<len(left)):
+        new_list.append(left[i])
+        i += 1
+    return new_list
+
+
+for i in sort(li):
+    print(i)
