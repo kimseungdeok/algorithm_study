6111ddc9e330454ea34947bdf8e145739cabc6c9
kimseungdeok <tmdejr1117@gmail.com>
2022-09-27 19:44:50 +0900

    resolved : 팩토리얼 재귀 적용

--- BOG/python/10872.py
+++ BOG/python/10872.py
@@ -3,15 +3,9 @@ import sys
 sys.stdin = open('example.txt', 'r')
 
 def factorial(num):
-    result = 1
-    if num==0:
+    if num == 1:
         return 1
-    else:
-        for i in range(1, num+1):
-            # print(i)
-            result = result * i
-
-    return result
+    return num * factorial(num-1)
 
 print(factorial(int(input())))
 # print(factorial(1))
