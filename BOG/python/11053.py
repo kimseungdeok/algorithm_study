165421fb8ebf6c7724ce05fe4308633879118999
kimseungdeok <tmdejr1117@gmail.com>
2022-10-17 14:23:22 +0900

    resolved : 11053 풀이 완료

---
+++ BOG/python/11053.py
@@ -0,0 +1,18 @@
+from sys import stdin
+
+stdin = open('example.txt', 'r')
+input = stdin.readline
+
+
+len = int(input())
+numbers = list(map(int, input().split()))
+
+
+dp = [1]* len
+
+for i in range(1,len):
+    for j in range(i):
+        if numbers[i] > numbers[j]:
+            dp[i] = max(dp[i],dp[j]+1)
+
+print(max(dp))
