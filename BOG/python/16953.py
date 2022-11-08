11e51009867909a7550aeff69963a193ac0c87ef
kimseungdeok <tmdejr1117@gmail.com>
2022-11-08 15:56:38 +0900

    resolved : b를 a로 만드는 방법으로 풀이

--- BOG/python/16953.py
+++ BOG/python/16953.py
@@ -3,28 +3,19 @@ from sys import stdin
 stdin = open('example.txt', 'r')
 input = stdin.readline
 
-
 a, b = map(int, input().split())
-count = 0
+count = 1
 
-min_count = 1e9
-
-def dfs(a, target, count):
-    global min_count
-
-    if a > target:
-        return
-
-    elif a == target:
-        min_count = min(min_count, count)
-        return
+while(b!=a):
+    count += 1
+    temp = b
+    if b%10 == 1:
+        b//=10
+    elif b%2 == 0:
+        b//=2
     
-    else:
-        dfs((a*2), target, count +1)
-        dfs((a*10+1), target, count+1)
-
-dfs(a,b,0)
-if min_count != 1e9:
-    print(min_count+1)
+    if temp == b:
+        print(-1)
+        break
 else:
-    print(-1)
+    print(count)
