eaf2dbc5399f006f72d126dd21b405f359a9e107
kimseungdeok <tmdejr1117@gmail.com>
2022-12-26 16:53:03 +0900

    resolved : 쿼드 압축 후 개수 세기 풀이 완료

---
+++ programmers/python/쿼드압축 후 개수 세기.py
@@ -0,0 +1,22 @@
+def solution(arr):
+    answer = [0, 0]
+    n = len(arr)
+    
+    def quard(x, y, n):
+        first = arr[x][y]
+        
+        for i in range(x, x + n):
+            for j in range(y, y + n):
+                if arr[i][j] != first:
+                    n //= 2
+                    quard(x, y, n)
+                    quard(x, y + n, n)
+                    quard(x + n, y, n)
+                    quard(x + n, y + n, n)
+                    return
+        
+        answer[first] += 1
+    
+    quard(0, 0, n)
+        
+    return (answer)
