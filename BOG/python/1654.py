ddd09653b114bdaaad2a658a3f51d8a4da808879
kimseungdeok <tmdejr1117@gmail.com>
2023-01-03 10:27:51 +0900

    resolved : 1654 풀이 완료

---
+++ BOG/python/1654.py
@@ -0,0 +1,27 @@
+from sys import stdin
+
+stdin = open('example.txt', 'r')
+input = stdin.readline
+
+k, n = map(int, input().split())
+lan = []
+for i in range(k):
+    lan.append(int(input()))
+
+start = 1
+end = max(lan)
+
+while start <= end:
+    mid = (start + end) // 2
+    count = 0
+    for i in lan:
+        count += i // mid
+    print(count)
+
+    if count >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
+
