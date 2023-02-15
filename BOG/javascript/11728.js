e918cd0aefcf3508f7e293d70fb1af2eb9302705
kimseungdeok <tmdejr1117@gmail.com>
2023-02-15 18:25:25 +0900

    failed : 11728 풀이

---
+++ BOG/javascript/11728.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+let filepath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
+let input = fs.readFileSync(filepath).toString().trim();
+input = input.split("\n");
+
+let answer = []
+// console.log(input.length)
+
+for(let i = 1; i < input.length; i++){
+    // console.log(i)
+    // console.log(input[i])
+    answer.push(input[i])
+}
+// console.log(answer[0].split(' '))
+
+for(let j = 0; j < answer.length; j++){
+    splits = answer[j].split(' ').map(Number)
+    answer[j] = splits
+}
+
+console.log(answer)
