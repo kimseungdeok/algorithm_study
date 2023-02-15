178c82396095b5494e8b82eeec42746c7e8d9032
kimseungdeok <tmdejr1117@gmail.com>
2023-02-15 18:42:28 +0900

    Chore : git 인증테스트 3

--- BOG/javascript/11728.js
+++ BOG/javascript/11728.js
@@ -3,21 +3,21 @@ let filepath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
 let input = fs.readFileSync(filepath).toString().trim();
 input = input.split("\n");
 
-let answer = []
+let answer = [];
 // console.log(input.length)
 
-for(let i = 1; i < input.length; i++){
-    // console.log(i)
-    // console.log(input[i])
-    answer.push(input[i])
+for (let i = 1; i < input.length; i++) {
+  // console.log(i)
+  // console.log(input[i])
+  answer.push(input[i]);
 }
 // console.log(answer[0].split(' '))
 
-for(let j = 0; j < answer.length; j++){
-    splits = answer[j].split(' ').map(Number)
-    answer[j] = splits
+for (let j = 0; j < answer.length; j++) {
+  splits = answer[j].split(" ").map(Number);
+  answer[j] = splits;
 }
 
-console.log(answer)
+console.log(answer);
 
-Ttest222
+Ttest2223333;
