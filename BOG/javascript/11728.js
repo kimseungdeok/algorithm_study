149a783f95a5ca8bb996ede1612cca0f5c3da250
kimseungdeok <tmdejr1117@gmail.com>
2023-02-15 21:50:57 +0900

    failed : 정답이지만 시간 초과

--- BOG/javascript/11728.js
+++ BOG/javascript/11728.js
@@ -4,25 +4,13 @@ let input = fs.readFileSync(filepath).toString().trim();
 input = input.split("\n");
 
 let answer = [];
-// console.log(input.length)
 
 for (let i = 1; i < input.length; i++) {
-  // console.log(i)
-  // console.log(input[i])
-  answer.push(input[i]);
-}
-// console.log(answer[0].split(' '))
-
-let r_answer = []
-
-for (let j = 0; j < answer.length; j++) {
-  splits = answer[j].split(" ").map(Number);
-  // console.log(splits)
-  r_answer.push(...splits)
-  // answer[j] = splits;
+  answer.push(...input[i].split(' ').map(Number));
 }
 
-r_answer.sort(function(a,b){
+answer.sort(function(a,b){
   return a-b;
 })
-console.log(...r_answer);
+
+console.log(...answer);
