8914e786d3dec1189e2bd1a64e40105fed8391cb
kimseungdeok <tmdejr1117@gmail.com>
2023-02-18 01:36:46 +0900

    resolved : 카드 뭉치 풀이 완료

--- programmers/javascript/카드 뭉치.js
+++ programmers/javascript/카드 뭉치.js
@@ -1,13 +1,23 @@
 function solution(cards1, cards2, goal) {
-    var answer = '';
-    
-    console.log(cards1, cards2, goal);
-    
-    const goalLen = goal.length;
-    
-    for(let i = 0; i < goalLen; i++){
-        
+    let answer = "Yes";
+    const cards1Length = cards1.length;
+    const cards2Length = cards2.length;
+    const goalLength = goal.length;
+    let one = 0;
+    let two = 0;
+
+    for (let i = 0; i < goalLength; i++) {
+        if (one < cards1Length && goal[i] === cards1[one]) {
+            one++;
+        } else if (two < cards2Length && goal[i] === cards2[two]) {
+            two++;
+        } else {
+            answer = "No";
+            break;
+        }
     }
-    
+
     return answer;
 }
+
+console.log(solution(["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"]))
