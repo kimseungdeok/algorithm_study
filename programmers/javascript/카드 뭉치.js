a4e66c613304cfa18d1940f201da7af17c74e3dc
kimseungdeok <tmdejr1117@gmail.com>
2023-02-18 00:36:44 +0900

    failed : 카드 뭉치 풀이

---
+++ programmers/javascript/카드 뭉치.js
@@ -0,0 +1,13 @@
+function solution(cards1, cards2, goal) {
+    var answer = '';
+    
+    console.log(cards1, cards2, goal);
+    
+    const goalLen = goal.length;
+    
+    for(let i = 0; i < goalLen; i++){
+        
+    }
+    
+    return answer;
+}
