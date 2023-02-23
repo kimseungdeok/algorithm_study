0e792b904f9a7eeacc082a4af233969098ef05c3
kimseungdeok <tmdejr1117@gmail.com>
2023-02-23 23:26:30 +0900

    failed : 합성수 찾기 시간 초과 실패

---
+++ programmers/javascript/합성수 찾기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    let answer = 0;
+    let arr = []
+    for (let i = 2; i <= n; i++){
+        arr.push(i)
+    }
+    // console.log(arr)
+    let sosu = []
+    
+    arr.forEach((item) => {
+        
+    })
+    
+    
+    
+    return answer;
+}
