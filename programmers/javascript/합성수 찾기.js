259b46047429c0a1e8044473376b64918dd237d6
kimseungdeok <tmdejr1117@gmail.com>
2023-02-23 23:49:39 +0900

    resolved : 합성수 찾기 풀이 완료

--- programmers/javascript/합성수 찾기.js
+++ programmers/javascript/합성수 찾기.js
@@ -1,17 +1,16 @@
 function solution(n) {
     let answer = 0;
-    let arr = []
-    for (let i = 2; i <= n; i++){
-        arr.push(i)
+    
+    for (let i = 1; i <= n; i++){
+        let cnt = 0;
+				// 자연수의 약수의 개수를 구하는 부분
+        for(j = 0; j <= i; j++){
+            if(i % j ===0) cnt++
+        }
+				// 약수의 개수가 2보다 크면(3이상이면) answer를 1증가시킴
+        if(cnt > 2) {
+            answer++
+        }
     }
-    // console.log(arr)
-    let sosu = []
-    
-    arr.forEach((item) => {
-        
-    })
-    
-    
-    
     return answer;
 }
