96ce78ed98ebe5d923820e42a10959d731023f00
kimseungdeok <tmdejr1117@gmail.com>
2023-02-27 18:35:23 +0900

    resolved : 숫자 짝꿍 풀이 완료

--- programmers/javascript/숫자 짝꿍.js
+++ programmers/javascript/숫자 짝꿍.js
@@ -1,16 +1,19 @@
 // 1. 같은 수를 찾고 배열에 넣음(twin이란 배열 하나 만들기)
 // 2. twin 배열을 sorting함(큰 수가 앞에 있게)
 function solution(X, Y) {
-    let answer = '';
-    let Xarr = [...X]
-    let Yarr = [...Y]
+    let answer = ''
+    X = X.split("")
+    Y = Y.split("")
     
-    const Xlen = Xarr.length;
-    const Ylen = Yarr.length;
-    for(let i = 0; i < Xlen; i++){
-        for(let j = 0; j < Ylen; j++){
-            if(Xarr[i] === Yarr[j])
-        }
+    // 해당 숫자가 얼마나 있는지 확인해서 더 낮은만큼 정답 문자열에 더함
+    for(let i = 0 ; i < 10 ; i ++) {
+        const curX = X.filter(a => Number(a) === i).length
+        const curY = Y.filter(a => Number(a) === i).length
+        answer+=String(i).repeat(Math.min(curX, curY))
     }
-    return answer;
+    
+    if(answer === '') return "-1"
+    if(Number(answer) === 0) return "0"
+    // 내림차순으로 정렬해 반환하면 최댓값
+    return answer.split("").sort((a,b) => Number(b)-Number(a)).join("")
 }
