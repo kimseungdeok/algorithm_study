af32553d46656343642a67be8e01c5610cbbacb1
kimseungdeok <tmdejr1117@gmail.com>
2023-02-27 16:30:34 +0900

    resolved : 소수 찾기

--- programmers/javascript/소수 찾기.js
+++ programmers/javascript/소수 찾기.js
@@ -1,9 +1,25 @@
 function solution(n) {
-    var answer = 0;
-    
-    let numbers = new Array(n).fill(0)
-    let 
-    for (let i = 0; i < )
-    
+    let answer = 0;
+
+    const numbers = new Array(n + 1).fill(-1);
+
+    for (let i = 2; i <= n; i++) {
+        if (numbers[i] === 0) {
+            continue;
+        }
+
+        for (let k = i + i; k <= n; k += i) {
+            numbers[k] = 0;
+        }
+    }
+
+    for (let i = 2; i <= n; i++) {
+        if (numbers[i] === -1) {
+            answer++;
+        }
+    }
+
     return answer;
 }
+
+console.log(solution(10));
