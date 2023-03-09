24e4afe278d938bd02598d8a005b6037a7adfd9a
kimseungdeok <tmdejr1117@gmail.com>
2023-03-09 22:24:06 +0900

    resolved : 풀이 완료

---
+++ programmers/javascript/숫자 문자열과 영단어.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
+
+    for(let i = 0; i < numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        console.log(arr)
+        answer = arr.join(i);
+    }
+
+    return Number(answer);
+}
+
+console.log(solution("one4seveneight"));
