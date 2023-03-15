0a6e1e3485580a77c3a0186856810ac6bbb0f667
kimseungdeok <tmdejr1117@gmail.com>
2023-03-16 02:01:19 +0900

    solved : 풀이 완료

---
+++ programmers/javascript/없는 숫자 더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    let answer = 0;
+    
+    for(let i = 0; i < 10; i++){
+        if(!numbers.includes(i)){
+
+            answer+= i
+        }
+    }
+    
+    return answer;
+    
+}
