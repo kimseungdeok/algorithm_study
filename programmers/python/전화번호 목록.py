a3b21944e37c119de2dfb4e8087588606eebbf9b
kimseungdeok <tmdejr1117@gmail.com>
2023-06-05 20:38:41 +0900

    resolved : 풀이 완료

--- programmers/python/전화번호 목록.py
+++ programmers/python/전화번호 목록.py
@@ -1,8 +1,9 @@
 def solution(phone_book):
     answer = True
-    first = phone_book.pop(0)
-
-    for i in phone_book:
-        if first in i:
-            return False
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if len(phone_book[i]) < len(phone_book[i+1]):
+            if phone_book[i + 1][:len(phone_book[i])] == phone_book[i]:
+                answer = False
+                break
     return answer
