f5e70575dc1237ba56b661b1e19cc267c622b226
kimseungdeok <tmdejr1117@gmail.com>
2023-06-18 22:54:39 +0900

    solved : 프로세스 풀이 완료

--- programmers/javascript/프로세스.js
+++ programmers/javascript/프로세스.js
@@ -1,20 +1,19 @@
 function solution(priorities, location) {
     let answer = 0;
-    // process와 index값 저장
-    const array = priorities.map((process,index) => {
-      return {process, index};
-    })
-  
-    while(array.length){
-      const queue = array.shift();
-      // some 메서드를 사용해서 queue.process 값보다 큰게 있는지 없는지 확인 있으면 push
-      if(array.some((element) => element.process > queue.process)){
-        array.push(queue);
-      }else{
-        // 없으면 answer++ index값이 location이랑 같아지면 break
-        answer++
-        if(queue.index === location) break;
-      }
+    const priorityAndIndex = priorities.map((priority, index) => {
+        return { priority, index };
+    });
+
+    while (priorityAndIndex.length) {
+        const process = priorityAndIndex.shift();
+
+        if (priorityAndIndex.some((el) => el.priority > process.priority)) {
+            priorityAndIndex.push(process);
+        } else {
+            answer++;
+            if (priorityAndIndex.index === location) break;
+        }
     }
+
     return answer;
-  }
+}
