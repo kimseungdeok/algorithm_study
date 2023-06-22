8d6756cb9483adaa5c0a8f04081fa901987fb9e2
kimseungdeok <tmdejr1117@gmail.com>
2023-06-22 20:59:14 +0900

    study : 최대 힙, 최소 힙 구현

--- programmers/javascript/maxHeap.js
+++ programmers/javascript/maxHeap.js
@@ -1,54 +1,100 @@
 class MaxHeap {
     constructor() {
-        this.heap = [null];
+        this.heap = [];
     }
 
-    heappush(value) {
-        this.heap.push(value);
-        let curIdx = this.heap.length - 1;
-        let parIdx = (curIdx / 2) >> 0;
+    getLeftChildIndex(parentIndex) {
+        return 2 * parentIndex + 1;
+    }
 
-        while (curIdx > 1 && this.heap[parIdx] < this.heap[curIdx]) {
-            [this.heap[parIdx], this.heap[curIdx]] = [
-                this.heap[parIdx],
-                this.heap[curIdx],
-            ];
-            curIdx = parIdx;
-            parIdx = (curIdx / 2) >> 0;
+    getRightChildIndex(parentIndex) {
+        return 2 * parentIndex + 2;
+    }
+
+    getParentIndex(childIndex) {
+        return Math.floor((childIndex - 1) / 2);
+    }
+
+    hasLeftChild(index) {
+        return this.getLeftChildIndex(index) < this.heap.length;
+    }
+
+    hasRightChild(index) {
+        return this.getRightChildIndex(index) < this.heap.length;
+    }
+
+    hasParent(index) {
+        return this.getParentIndex(index) >= 0;
+    }
+
+    leftChild(index) {
+        return this.heap[this.getLeftChildIndex(index)];
+    }
+
+    rightChild(index) {
+        return this.heap[this.getRightChildIndex(index)];
+    }
+
+    parent(index) {
+        return this.heap[this.getParentIndex(index)];
+    }
+
+    swap(index1, index2) {
+        [this.heap[index1], this.heap[index2]] = [
+            this.heap[index2],
+            this.heap[index1],
+        ];
+    }
+
+    peek() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+        return this.heap[0];
+    }
+
+    poll() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+        if (this.heap.length === 1) {
+            return this.heap.pop();
+        }
+        const item = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown();
+        return item;
+    }
+
+    add(item) {
+        this.heap.push(item);
+        this.heapifyUp();
+    }
+
+    heapifyUp() {
+        let index = this.heap.length - 1;
+        while (this.hasParent(index) && this.parent(index) < this.heap[index]) {
+            const parentIndex = this.getParentIndex(index);
+            this.swap(index, parentIndex);
+            index = parentIndex;
         }
     }
 
-    heappop() {
-        const max = this.heap[1];
-        if (this.heap.length <= 2) this.heap[null];
-        else this.heap[1] = this.heap.pop();
-
-        let curIdx = 1;
-        let leftIdx = curIdx * 2;
-        let rightIdx = curIdx * 2 + 1;
-
-        if (!this.heap[leftIdx]) return max;
-
-        if (!this.heap[rightIdx]) {
-            if (this.heap[leftIdx] > this.heap[curIdx]) {
-                [this.heap[leftIdx], this.heap[curIdx]] = [
-                    this.heap[curIdx],
-                    this.heap[leftIdx],
-                ];
+    heapifyDown() {
+        let index = 0;
+        while (this.hasLeftChild(index)) {
+            let largerChildIndex = this.getLeftChildIndex(index);
+            if (
+                this.hasRightChild(index) &&
+                this.rightChild(index) > this.leftChild(index)
+            ) {
+                largerChildIndex = this.getRightChildIndex(index);
             }
-            return min;
+            if (this.heap[index] > this.heap[largerChildIndex]) {
+                break;
+            }
+            this.swap(index, largerChildIndex);
+            index = largerChildIndex;
         }
-
-        while (
-            this.heap[leftIdx] > this.heap[curIdx] ||
-            this.heap[rightIdx] > this.heap[curIdx]
-        ) {
-            const maxIdx =
-                this.heap[leftIdx] < this.heap[rightIdx] ? rightIdx : leftIdx;
-            curIdx = maxIdx;
-            leftIdx = curIdx * 2;
-            rightIdx = curIdx * 2 + 1;
-        }
-        return max
     }
 }
