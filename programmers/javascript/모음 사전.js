6d5a5e251d4e77dc68e4b880d252dd92e5f516d3
kimseungdeok <tmdejr1117@gmail.com>
2023-07-02 03:14:28 +0900

    docs : add comment

--- programmers/javascript/모음 사전.js
+++ programmers/javascript/모음 사전.js
@@ -1,15 +1,24 @@
+// 문제 설명
+// 모음만 있는 사전이 있다.
+// 주어진 단어가 사전에서 몇 번째인지를 return 하는 함수를 만드는 문제이다.
+
+// 예시
+// 단어 : "AAAAE"
+// 사전에서의 순서 : 6
+
 function calculateOrder(num) {
     let sum = 1;
     for (let i = 1; i <= num; i++) {
-        sum += 5 ** i;
+        sum += 5 ** i; // num 값에 따라 5의 거듭제곱을 sum에 더해준다.
     }
-    return sum;
+    return sum; // 최종적으로 계산된 sum 값을 반환한다.
 }
 
 function solution(word) {
     let answer = 0;
 
     const aeiou = {
+        // 각 모음(A, E, I, O, U)에 대한 인덱스 값을 저장한 객체를 선언한다.
         A: 0,
         E: 1,
         I: 2,
@@ -18,8 +27,9 @@ function solution(word) {
     };
 
     word.split("").map((char, index) => {
-        answer += calculateOrder(4 - index) * aeiou[char] + 1;
+        // word 문자열을 하나씩 나누어서 반복한다.
+        answer += calculateOrder(4 - index) * aeiou[char] + 1; // calculateOrder 함수와 aeiou 객체를 이용하여 계산을 수행한다.
     });
 
-    return answer;
+    return answer; // 계산된 answer 값을 반환한다.
 }
